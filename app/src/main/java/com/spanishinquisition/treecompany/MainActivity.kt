a9ec65a6a1b803b9c8581f52438485ef580ffbc1
Sacha Buelens <sacha.buelens@student.kdg.be>
2019-04-05 13:09:29 +0200

    Setup android
    Setting up the first layouts

--- app/src/main/java/com/spanishinquisition/treecompany/MainActivity.kt
+++ app/src/main/java/com/spanishinquisition/treecompany/MainActivity.kt
@@ -2,11 +2,15 @@ package com.spanishinquisition.treecompany
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.decorView.apply {
+            systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
     }
 }
