4370b43193cb28d3556a7c3a67e22cc22f616672
Edwin <edwinkaiyin.tam@student.kdg.be>
2019-05-09 16:31:49 +0200

    Create homescreen branch commit

--- app/src/main/java/com/spanishinquisition/treecompany/activitties/HomeScreenActivity.kt
+++ app/src/main/java/com/spanishinquisition/treecompany/activitties/HomeScreenActivity.kt
@@ -1,7 +1,10 @@
 package com.spanishinquisition.treecompany.activitties
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.spanishinquisition.treecompany.R
 
 class HomeScreenActivity : Activity() {
@@ -9,5 +12,19 @@ class HomeScreenActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_layout)
+        setSupportActionBar(findViewById(R.id.home_toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.info -> {
+            startActivity(Intent(this, AppInfoActivity::class.java))
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 }
