4839171849d64c764f72f44a7849a68c9850a1d5
Edwin <edwinkaiyin.tam@student.kdg.be>
2019-05-12 13:57:03 +0200

    Splash timeout alert

--- app/src/main/java/com/spanishinquisition/treecompany/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/spanishinquisition/treecompany/activities/SplashScreenActivity.kt
@@ -1,9 +1,11 @@
 package com.spanishinquisition.treecompany.activities
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import com.spanishinquisition.treecompany.R
@@ -14,19 +16,16 @@ class SplashScreenActivity : Activity() {
     private lateinit var loadingTv: TextView
     private lateinit var imageHome: ImageView
 
+    private var isConnected: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-//        window.decorView.apply {
-//            systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-//        }
         initialiseViews()
-        runSplash()
+        testConnection()
     }
 
     private fun initialiseViews() {
-
         loadingTv = findViewById(R.id.loadingCopyright)
         val stringList = resources.getStringArray(R.array.loadingScreen)
         loadingTv.text = stringList[Random.nextInt(stringList.size - 1)]
@@ -34,18 +33,42 @@ class SplashScreenActivity : Activity() {
         imageHome.setImageResource(R.drawable.logo)
     }
 
+    private fun testConnection() {
+        if (RestClient(this).isConnectedToServer())
+            runSplash()
+        else
+            showAlertDialog(this, R.string.connection_title,R.string.connection_msg)
+    }
+
     private fun runSplash() {
         val t = Thread {
-            Thread.sleep(5000)
-            if (RestClient(this).isConnectedToServer()) {
-                startActivity(Intent(this, MainActivity::class.java))
-                finish()
-            } else {
-                // TODO add alert with connectivity problems
-                finish()
+            run {
+                try {
+                    Thread.sleep(5000)
+                } catch (e: InterruptedException) {
+                    Log.d("Error", e.toString())
+                } finally {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                }
             }
         }
         t.start()
+    }
+
+    private fun showAlertDialog(context: Context, title: Int, message: Int) {
+        val builder: AlertDialog.Builder = context.let {
+            AlertDialog.Builder(it)
+        }
+
+        builder.apply {
+            setMessage(message)
+            setTitle(title)
+            setPositiveButton(R.string.connection_dialog_retry) { _, _ -> testConnection() }
+            setNegativeButton(R.string.connection_dialog_close) { _, _ -> finish() }
+        }
+
+        builder.create().show()
     }
 }
 
